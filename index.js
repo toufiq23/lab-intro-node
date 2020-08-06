614a71944712aae310f8fc18da98d52abe5a5a2c
Enine Chareunsouk <enine.chareunsouk@gmail.com>
2020-08-06 21:46:18 +0200

    all done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class SortedList {
-  constructor() {
+  constructor() { 
 		this.items = [];
 		this.length = 0;
 	}
@@ -10,15 +10,43 @@ class SortedList {
 		this.length++;
 	}
 
-  get(pos) {}
+  get(pos) {
+    if(pos >= this.length){
+      throw new Error('OutOfBounds');
+      
+    }
+    return this.items[pos]
+  }  
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items);
+      
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    if(this.length === 0){
+      return 0;
+    }
+    return this.items.reduce((a,b) => a+b);
+  }
 
-  avg() {}
+
+  avg() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items.reduce((a,b) => a+b)/this.length;
+  }
 }
 
 module.exports = SortedList;
